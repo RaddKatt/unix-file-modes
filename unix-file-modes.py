d8bb5ce597d7fe944c8c06798d0f325ba61d4a84
RaddKatt <raddkatt0@gmail.com>
2020-05-19 20:57:45 -0700

    Updated to octal

--- unix-file-modes.py
+++ unix-file-modes.py
@@ -6,10 +6,10 @@ for i in mode_start:
     for j in range(0,8):
         for k in range(0,8):
             for l in range(0,8):
-                mode_num = str(i) + str(j) + str(k) + str(l)
+                mode_num = str(0) + str(i) + str(j) + str(k) + str(l)
                 mode_hr = ['-','-','-']
-                for m in range(1, len(mode_num)):
-                    mode_index = m - 1
+                for m in range(2, len(mode_num)):
+                    mode_index = m - 2
                     if mode_num[m] == '0':
                         perms = '---'
                         mode_hr[mode_index] = perms
@@ -36,11 +36,11 @@ for i in mode_start:
                         mode_hr[mode_index] = perms
                 mode_hr = ''.join(mode_hr)
                 mode_hr = list(mode_hr)
-                if mode_num[0] == '1':
+                if mode_num[1] == '1':
                     mode_hr[8] = 't'
-                if mode_num[0] == '2':
+                if mode_num[1] == '2':
                     mode_hr[5] = 's'
-                if mode_num[0] == '4':
+                if mode_num[1] == '4':
                     mode_hr[2] = 's'
                 mode_hr = ''.join(mode_hr)
                 print(mode_num, mode_hr)
